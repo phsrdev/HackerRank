5392e468a6d3208e8bb2f117ad8e3a106f1655ab
Pedro Rodrigues <phsr@live.com>
2022-11-25 21:27:35 +0000

    Exercicio29 update

--- Python/TextAlignment.py
+++ Python/TextAlignment.py
@@ -1,27 +1,23 @@
 #Replace all ______ with rjust, ljust or center. 
 
-thickness = int(input()) #This must be an odd number
-c = 'H'
-
-#Top Cone
-for i in range(thickness):
-    print((c*i).______(thickness-1)+c+(c*i).______(thickness-1))
-
-#Top Pillars
-for i in range(thickness+1):
-    print((c*thickness).______(thickness*2)+(c*thickness).______(thickness*6))
-
-#Middle Belt
-for i in range((thickness+1)//2):
-    print((c*thickness*5).______(thickness*6))    
-
-#Bottom Pillars
-for i in range(thickness+1):
-    print((c*thickness).______(thickness*2)+(c*thickness).______(thickness*6))    
-
-#Bottom Cone
-for i in range(thickness):
-    print(((c*(thickness-i-1)).______(thickness)+c+(c*(thickness-i-1)).______(thickness)).______(thickness*6))
+width = int(input()) #widths is odd and 0 < width < 50
+t = width*2
+c = "H"
+#top arrow
+for a in range(1,t,2):
+    print((c*a).center(t))
+#top pillars
+for a in range(width+1):
+     print((c*width).center(t)+" ".center(t)+(c*width).center(t))
+#center
+for a in range((width+1)//2):
+     print((c*width*5).center(t*3))
+#bottom pillars
+for a in range(width+1):
+     print((c*width).center(t)+" ".center(t)+(c*width).center(t))
+#bottom arrow
+for a in reversed(range(1,t,2)):
+    print(" ".center(t*2)+(c*a).center(t))
 
 '''
 In Python, a string of text can be aligned left, right and center.
