32c8fa541490eba6aa9ba25ac1198f45b2b5ad05
Pedro Rodrigues <phsr@live.com>
2022-11-28 15:32:39 +0000

    Exercicio26

--- Python/NoIdea.py
+++ Python/NoIdea.py
@@ -1,3 +1,17 @@
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+
+happiness = 0
+
+for i in arr:
+    if i in A:
+        happiness += 1
+    if i in B:
+        happiness += -1
+print(happiness)
+
 '''
 There is an array of n integers. There are also 2 disjoint sets, A and B, each containing m integers. You like all the integers in set A and dislike all the integers in set B. Your initial happiness is 0. For each i integer in the array, if i ∈ A, you add 1 to your happiness. If i ∈ B, you add -1 to your happiness. Otherwise, your happiness does not change. Output your final happiness at the end.
 
