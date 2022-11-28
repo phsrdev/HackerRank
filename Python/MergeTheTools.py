3216426c2302a3dbe9e35024a9ee46b663bf4a02
Pedro Rodrigues <phsr@live.com>
2022-11-28 15:30:24 +0000

    Exercicio25

--- Python/MergeTheTools.py
+++ Python/MergeTheTools.py
@@ -1,3 +1,18 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    n = len(string)
+    for i in range(0, n, k):
+        subsequence = ""
+        substring = string[i:(i + k)]
+        for l in substring:
+            if l not in subsequence:
+                subsequence += l
+        print(subsequence)
+        
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
+
 '''
 Consider the following:
 
